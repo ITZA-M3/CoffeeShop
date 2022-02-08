ae6622b34578dac379f4cec5c7fe37d91368082e
Jess <45025366+ITZA-M3@users.noreply.github.com>
2022-02-08 12:41:11 -0500

    Figured out why the content wasn't loading properly

--- CoffeeShop/Game1.cs
+++ CoffeeShop/Game1.cs
@@ -3,13 +3,27 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace CoffeeShop
-{
+{    public enum GameState
+    {
+        title,
+        game,
+        pause,
+        menu
+    }
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        #region Textures
         private Texture2D title;
+        #endregion
+
+        private GameState currState;
+
+        // used to hold game window measurements
+        private int width;
+        private int height;
 
         public Game1()
         {
@@ -22,7 +36,13 @@ namespace CoffeeShop
         {
             // TODO: Add your initialization logic here
 
-           
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.PreferredBackBufferWidth = 960;
+
+            width = _graphics.GraphicsDevice.Viewport.Width;
+            height = _graphics.GraphicsDevice.Viewport.Height;
+
+            currState = GameState.title;
 
             base.Initialize();
         }
@@ -32,24 +52,51 @@ namespace CoffeeShop
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            //title = this.Content.Load<Texture2D>("CoffeeTitle");
+            title = this.Content.Load<Texture2D>("CoffeeTitle");
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
+            // switches scenes
+            switch (currState)
+            {
+                case GameState.title:
+                    break;
+                case GameState.game:
+                    break;
+                case GameState.menu:
+                    break;
+                case GameState.pause:
+                    break;
+                default:
+                    break;
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
+            _spriteBatch.Begin();
 
-            // TODO: Add your drawing code here
+            // draws the current scene
+            switch (currState)
+            {
+                case GameState.title:
+                    _spriteBatch.Draw(title, new Rectangle(0, 0, width, height), Color.White);
+                    break;
+                case GameState.game:
+                    break;
+                case GameState.menu:
+                    break;
+                case GameState.pause:
+                    break;
+                default:
+                    break;
+            }
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
